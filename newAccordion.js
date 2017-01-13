98c096011b3dd2d26fc8cecfddaa23bd60e5c2e1
Kevin Salazar <kskevin170@yahoo.com>
2017-01-13 13:01:11 -0800

    jquery work

--- newAccordion.js
+++ newAccordion.js
@@ -1,28 +1,117 @@
-  $('.fit-navbar').on("click", function() {
-    $(".fit-content").toggleClass("hidden")
-    $('.fit-navbar').toggleClass("responsive")
-    $('.accordion-icon').html('<span class="accordion-icon"> - </span>')
+$(document).ready(function() {
+  // Variables for desktop design
+  var navbr = $(".navbr");
+  var navbar = $(".navbar");
+  var item = this;
+  var fit_content = $(".fit-content");
+  var care_content = $(".care-content");
+  var materials_content = $(".materials-content");
+  var navbar_fit_button = $(".fit-navbar");
+  var navbar_care_button = $(".care-navbar");
+  var navbar_materials_button = $(".materials-navbar");
+  // Variables for responsive design
+  var accordion_icon = $(".accordion-icon");
+  var phone_navbar = $(".phone-navbar");
+  var accordion_icon_fit = $("#accordion-icon-fit");
+  var accordion_icon_care = $("#accordion-icon-care");
+  var accordion_icon_materials = $("#accordion-icon-materials");
 
-    $('.care-navbar').removeClass("responsive")
-    $('.materials-navbar').removeClass("responsive")
-    $(".care-content").addClass("hidden")
-    $(".materials-content").addClass("hidden")
-  })
-  $('.care-navbar').on("click", function() {
-    $(".care-content").toggleClass("hidden")
-    $('.care-navbar').toggleClass("responsive")
+  // navbar_fit_button.on("click", function() {
+  //   fit_content.toggleClass("hidden")
+  //   navbar_fit_button.toggleClass("responsive")
+  //   accordion_icon_fit.html("-");
+  //   navbar_fit_button.toggleClass('navbar__button--active')
+  //
+  //   if (phone_navbar.hasClass("responsive")) {
+  //         accordion_icon.html("-");
+  //     } else {
+  //         accordion_icon.html("+");
+  //     }
+  //
+  //   navbar_care_button.removeClass('navbar__button--active')
+  //   navbar_materials_button.removeClass('navbar__button--active')
+  //
+  //
+  //   accordion_icon_care.html("+");
+  //   accordion_icon_materials.html("+");
+  //   navbar_care_button.removeClass("responsive")
+  //   navbar_materials_button.removeClass("responsive")
+  //   care_content.addClass("hidden")
+  //   materials_content.addClass("hidden")
+  // })
+  // navbar_care_button.on("click", function() {
+  //   care_content.toggleClass("hidden")
+  //   navbar_care_button.toggleClass("responsive")
+  //   accordion_icon_care.html("-");
+  //   navbar_care_button.toggleClass('navbar__button--active')
+  //
+  //
+  //   if (phone_navbar.hasClass("responsive")) {
+  //         accordion_icon.html("-");
+  //     } else {
+  //         accordion_icon.html("+");
+  //     }
+  //
+  //   navbar_fit_button.removeClass('navbar__button--active')
+  //   navbar_materials_button.removeClass('navbar__button--active')
+  //
+  //
+  //   accordion_icon_materials.html("+");
+  //   accordion_icon_fit.html("+");
+  //   navbar_fit_button.removeClass("responsive")
+  //   navbar_materials_button.removeClass("responsive")
+  //   fit_content.addClass("hidden")
+  //   materials_content.addClass("hidden")
+  // })
+  // navbar_materials_button.on("click", function() {
+  //   materials_content.toggleClass("hidden")
+  //   navbar_materials_button.toggleClass("responsive")
+  //   accordion_icon_materials.html("-");
+  //   navbar_materials_button.toggleClass('navbar__button--active')
+  //
+  //
+  //   if (phone_navbar.hasClass("responsive")) {
+  //         accordion_icon.html("-");
+  //     } else {
+  //         accordion_icon.html("+");
+  //     }
+  //
+  //   navbar_fit_button.removeClass('navbar__button--active')
+  //   navbar_care_button.removeClass('navbar__button--active')
+  //
+  //
+  //   accordion_icon_care.html("+");
+  //   accordion_icon_fit.html("+");
+  //   navbar_fit_button.removeClass("responsive")
+  //   navbar_care_button.removeClass("responsive")
+  //   care_content.addClass("hidden")
+  //   fit_content.addClass("hidden")
+  // })
 
-    $('.fit-navbar').removeClass("responsive")
-    $('.materials-navbar').removeClass("responsive")
-    $(".fit-content").addClass("hidden")
-    $(".materials-content").addClass("hidden")
-  })
-  $('.materials-navbar').on("click", function() {
-    $(".materials-content").toggleClass("hidden")
-    $('.materials-navbar').toggleClass("responsive")
 
-    $('.fit-navbar').removeClass("responsive")
-    $('.care-navbar').removeClass("responsive")
-    $(".care-content").addClass("hidden")
-    $(".fit-content").addClass("hidden")
+
+
+
+
+
+  $('button').on("click", function() {
+    if($(this).hasClass("fit-navbar")) {
+
+    } else if ($(this).hasClass("care-navbar")) {
+
+    } else if ($(this).hasClass("materials-navbar")) {
+      
+    }
+
   })
+
+
+
+
+
+
+
+
+
+
+})
