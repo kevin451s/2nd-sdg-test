a4d43a30cb67d90272d4250e2b304002e986873e
Kevin Salazar <kskevin170@yahoo.com>
2017-01-18 10:04:31 -0800

    just a commit

--- newAccordion.js
+++ newAccordion.js
@@ -1,161 +1,72 @@
-$(document).ready(function() {
-  // Variables for desktop design
-  var navbr = $(".navbr");
-  var navbar = $(".navbar");
-  var fit_content = $(".fit-content");
-  var care_content = $(".care-content");
-  var materials_content = $(".materials-content");
-  var navbar_fit_button = $(".fit-navbar");
-  var navbar_care_button = $(".care-navbar");
-  var navbar_materials_button = $(".materials-navbar");
-  // Variables for responsive design
-  var accordion_icon = $(".accordion-icon");
-  var phone_navbar = $(".phone-navbar");
-  var accordion_icon_fit = $("#accordion-icon-fit");
-  var accordion_icon_care = $("#accordion-icon-care");
-  var accordion_icon_materials = $("#accordion-icon-materials");
+var active=".active",
+    id="#info",
+    btn=".js-btn",
+    content = ".js-content";
 
-  // navbar_fit_button.on("click", function() {
-  //   fit_content.toggleClass("hidden")
-  //   navbar_fit_button.toggleClass("responsive")
-  //   accordion_icon_fit.html("-");
-  //   navbar_fit_button.toggleClass('navbar__button--active')
-  //
-  //   if (phone_navbar.hasClass("responsive")) {
-  //         accordion_icon.html("-");
-  //     } else {
-  //         accordion_icon.html("+");
-  //     }
-  //
-  //   navbar_care_button.removeClass('navbar__button--active')
-  //   navbar_materials_button.removeClass('navbar__button--active')
-  //
-  //
-  //   accordion_icon_care.html("+");
-  //   accordion_icon_materials.html("+");
-  //   navbar_care_button.removeClass("responsive")
-  //   navbar_materials_button.removeClass("responsive")
-  //   care_content.addClass("hidden")
-  //   materials_content.addClass("hidden")
-  // })
-  // navbar_care_button.on("click", function() {
-  //   care_content.toggleClass("hidden")
-  //   navbar_care_button.toggleClass("responsive")
-  //   accordion_icon_care.html("-");
-  //   navbar_care_button.toggleClass('navbar__button--active')
-  //
-  //
-  //   if (phone_navbar.hasClass("responsive")) {
-  //         accordion_icon.html("-");
-  //     } else {
-  //         accordion_icon.html("+");
-  //     }
-  //
-  //   navbar_fit_button.removeClass('navbar__button--active')
-  //   navbar_materials_button.removeClass('navbar__button--active')
-  //
-  //
-  //   accordion_icon_materials.html("+");
-  //   accordion_icon_fit.html("+");
-  //   navbar_fit_button.removeClass("responsive")
-  //   navbar_materials_button.removeClass("responsive")
-  //   fit_content.addClass("hidden")
-  //   materials_content.addClass("hidden")
-  // })
-  // navbar_materials_button.on("click", function() {
-  //   materials_content.toggleClass("hidden")
-  //   navbar_materials_button.toggleClass("responsive")
-  //   accordion_icon_materials.html("-");
-  //   navbar_materials_button.toggleClass('navbar__button--active')
-  //
-  //
-  //   if (phone_navbar.hasClass("responsive")) {
-  //         accordion_icon.html("-");
-  //     } else {
-  //         accordion_icon.html("+");
-  //     }
-  //
-  //   navbar_fit_button.removeClass('navbar__button--active')
-  //   navbar_care_button.removeClass('navbar__button--active')
-  //
-  //
-  //   accordion_icon_care.html("+");
-  //   accordion_icon_fit.html("+");
-  //   navbar_fit_button.removeClass("responsive")
-  //   navbar_care_button.removeClass("responsive")
-  //   care_content.addClass("hidden")
-  //   fit_content.addClass("hidden")
-  // })
+var accordion = function() {
+  $(id).on('click',btn,function() {
+      closeAccordion();
+      openAccordion($(this));
+  });
+};
+var openAccordion = function(btn) {
+    var info = btn.data('info');
+    btn.addClass(active).addClass('content__btn--active').addClass("bottom");
+    $('#'+info).show();
+};
+
+var closeAccordion = function() {
+    $(id).find(btn).removeClass(active).removeClass('content__btn--active').removeClass('bottom');
+    $(id).find(content).hide();
+}
+
+// Variables for desktop design
+accordion();
 
 
 
 
 
-
-
-  $('button').on("click", function() {
-    if($(this).hasClass("fit-navbar")) {
-      if(fit_content.hasClass("hidden")) {
-        fit_content.removeClass("hidden")
-        // Highlights Active Tab
-        navbar_fit_button.addClass("navbar__button--active");
-
-        // Removes Active Tab
-        navbar_materials_button.removeClass("navbar__button--active");
-        navbar_care_button.removeClass("navbar__button--active");
-
-        // Hides Content Divs
-        care_content.addClass("hidden");
-        materials_content.addClass("hidden");
-      } else {
-        fit_content.addClass("hidden");
-        navbar_fit_button.removeClass("navbar__button--active");
-      }
-    } else if ($(this).hasClass("care-navbar")) {
-      if(care_content.hasClass("hidden")) {
-        care_content.removeClass("hidden")
-        // Highlights Active Tab
-        navbar_care_button.addClass("navbar__button--active");
-
-        // Removes Active Tab
-        navbar_materials_button.removeClass("navbar__button--active ");
-        navbar_fit_button.removeClass("navbar__button--active ");
-
-        // Hides Content Divs
-        fit_content.addClass("hidden");
-        materials_content.addClass("hidden");
-      } else {
-        care_content.addClass("hidden");
-        navbar_care_button.removeClass("navbar__button--active")
-      }
-    } else if ($(this).hasClass("materials-navbar")) {
-      if(materials_content.hasClass("hidden")) {
-        materials_content.removeClass("hidden")
-        // Highlights Active Tab
-        navbar_materials_button.addClass("navbar__button--active");
-
-        // Removes Active Tab
-        navbar_care_button.removeClass("navbar__button--active ");
-        navbar_fit_button.removeClass("navbar__button--active ");
-
-        // Hides Content Divs
-        fit_content.addClass("hidden");
-        care_content.addClass("hidden");
-      } else {
-        materials_content.addClass("hidden");
-        navbar_materials_button.removeClass("navbar__button--active");
-      }
-    }
-
-  })
-
-
-
-
-
-
-
-
-
-
-})
+// var accordion = function(opts) {
+//     var config = {
+//         active : ".active",
+//         id : "#info",
+//         btn : ".js-btn",
+//         content : ".js-content"
+//     },
+//
+//     c = $.extend(config,opts),
+//
+//     init = function() {
+//         console.log(c);
+//         btnClick();
+//     },
+//
+//     btnClick = function() {
+//         $(c.id).on('click',c.btn,function() {
+//             accordionClose();
+//             accordionOpen($(this));
+//         });
+//     },
+//
+//     accordionOpen = function(btn) {
+//         var info = btn.data('info');
+//         btn.addClass(c.active);
+//         $('#'+info).show();
+//     },
+//
+//     accordionClose = function() {
+//         $(c.id).find(c.btn).removeClass(c.active);
+//         $(c.id).find(c.content).hide();
+//     };
+//
+//     return {
+//         init : init,
+//         close : accordionClose
+//     }
+// };
+//
+// var newAccordion = accordion({
+//     btn : '.js-whaaat'
+// });
+// newAccordion.init();
